a0d840b67004bc1ba6956a05877db4187339b20b
VladBieliaiev <vlaaab9@gmail.com>
2022-09-04 02:12:35 +0200

    finished toDo (with localstorage), added deletion and execution of listItems

--- app.js
+++ app.js
@@ -2,45 +2,105 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const input = document.getElementById("task-description");
 const taskList = document.getElementById("task-list");
 const checkbox = document.querySelector(".btn-complete");
-const taskItem = document.querySelector('.task-item__description');
+
 
 console.log(taskList);
-console.log(taskItem)
 
-let tasks = [];
+let tasks;
+
+
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
+
+let taskItem = [];
 
 function Task(description) {
    this.description = description;
-   this.complete = false;
+   this.completed = false;
 }
 
 
-addTaskBtn.addEventListener('click', () => {
-   tasks.push(new Task(input.value))
-   input.value = '';
-   console.log(tasks);
+
+input.addEventListener('keypress', (e) => {
+   if (input.value != '' && e.key === 'Enter') {
+      tasks.push(new Task(input.value))
+      input.value = '';
+   }
+   updateLocalStorage();
    fillTaskList();
+   filterTasks();
+
 })
 
-function fillTaskList() {
-   taskList.innerHTML = "";
-   tasks.map(i => {
-      taskList.innerHTML += `
-      <div class="todo-task__list" id="task-list">
-         <div class="task-item">
-            <div class="task-item__description">${i.description}</div>
-            <div class="buttons">
-               <input type="checkbox" class="btn-complete">
-               <button class="task-item__delete"><img src="/img/delete.svg" alt=""></button>
-            </div>
+addTaskBtn.addEventListener('click', () => {
+   if (input.value != '') {
+      tasks.push(new Task(input.value))
+      input.value = '';
+   }
+   updateLocalStorage();
+   fillTaskList();
+   filterTasks();
+
+})
+
+const createTaskItem = (task, index) => {
+   return `
+      <div class="task-item ${task.completed ? "done" : ''}">
+         <div class="task-item__description" >${task.description}</div>
+         <div class="buttons">
+            <input onclick="completeTask(${index})" type="checkbox"${task.completed ? "checked" : ""} class="btn-complete">
+            <button onclick="deleteTask(${index})" id="delBtn" class="task-item__delete"><img src="/img/delete.svg" alt=""></button>
          </div>
       </div>`
-   })
-
 }
 
 
 
+const fillTaskList = () => {
+   taskList.innerHTML = "";
+   if (tasks.length > 0) {
+      tasks.map((i, index) => {
+         taskList.innerHTML += createTaskItem(i, index)
+      })
+      taskItem = document.querySelectorAll('.task-item')
+   }
+}
+
+const filterTasks = () => {
+   const activeTasks = tasks.filter(item => item.completed == false);
+   const completedTasks = tasks.filter(item => item.completed == true);
+   tasks = [...activeTasks, ...completedTasks];
+   updateLocalStorage();
+   fillTaskList();
+}
+
+
+const completeTask = index => {
+   tasks[index].completed = !tasks[index].completed
+   if (!taskItem.completed) {
+      taskItem[index].classList.add('done');
+   }
+   if (taskItem.completed) {
+      taskItem[index].classList.remove('done');
+   }
+   updateLocalStorage();
+   fillTaskList();
+   filterTasks();
+}
+
+
+const deleteTask = index => {
+   console.log(index);
+   tasks.splice(index, 1);
+   updateLocalStorage();
+   fillTaskList();
+}
+const updateLocalStorage = () => {
+   localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+filterTasks();
+fillTaskList();
 
 
 
@@ -75,8 +135,6 @@ function fillTaskList() {
 
 
 
-
-// !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
 
 // console.log(tasks);
 
